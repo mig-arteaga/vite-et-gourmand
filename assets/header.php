4d2100f7fdca888605cf62f8dd01b13665f81531
Miguel Arteaga <maaobarsa2@gmail.com>
2026-07-20 16:26:42 +0200

    Création de header.php

---
+++ assets/header.php
@@ -0,0 +1,29 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <nav class="nav">
+            <a href="index.php">
+                <img src="images/vg-logo.png" alt="" class="title-img">
+            </a>
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link active">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Menus</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Nous</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="login">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -4,44 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Rosa</title>
+    <title>Accueil - Vite & Gourmand</title>
     <!--Font awesome CDN-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.0/css/all.min.css">
     <!--Scroll reveal CDN-->
     <script src="https://unpkg.com/scrollreveal"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <!-- Header -->
-    <header>
-        <div class="container">
-            <nav class="nav">
-                <!-- <div class="toggle-menu" onclick="toggleMenu()">
-                    <div class="icon-box">
-                      <i class="top-line"></i>
-                      <i class="middle-line"></i>
-                      <i class="bottom-line"></i>
-                    </div>
-                </div> -->
-                <a href="index.php">
-                    <img src="images/logo-rosa.png" alt="" class="title-img">
-                </a>
-                <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="index.php" class="nav-link active">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Menus</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Nous</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <?php
+        require "assets/header.php";
+    ?>
 </body>
 </html>
